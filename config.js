b8a68f8e6a1adc48749d65bb9be882c10370b947
Yassine Addi <yasott@gmail.com>
2016-10-26 13:53:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 {
-	"VirusTotal_apikey": "99b3de5a67bbc64c922eedfa09fc0e281c5fa6a39b8c873ce11d5806b3e7c213",
-	"UnPHP_apikey": "ceb2e9e59d976e9c859e2e13af06ffca"
+	"VirusTotal_apikey": "",
+	"UnPHP_apikey": ""
 }
